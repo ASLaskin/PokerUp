aa8981b88283d0f4526d4b8ec306aedf1a2d789d
ASLaskin <andrewlaskin@ufl.edu>
2024-05-09 15:25:50 -0400

    qol changes

--- client/src/pages/game/play.jsx
+++ client/src/pages/game/play.jsx
@@ -24,39 +24,42 @@ function Play({ socket }) {
 			socket.emit('requestCards', location.state.name, updatedPlayers);
 		}
 	}
-    function handleCardsDealt({ cards }) {
-        console.log('Cards dealt', cards);
-        setCurrentCards(cards);
-    }
+	function handleCardsDealt({ cards }) {
+		console.log('Cards dealt', cards);
+		setCurrentCards(cards);
+	}
 
-    return (
-        <>
-            <div className="container mx-auto px-4 py-8">
-                <div className="text-2xl font-bold mb-4">Play</div>
-                <div className="mb-2">Room Name: {location.state.name}</div>
-                <div className="mb-2">Players:</div>
-                <ul className="mb-4">
-                    {players.map((playerId, index) => (
-                        <li key={index} className="mb-1">{playerId}</li>
-                    ))}
-                </ul>
-                <div className="mb-2">Cards:</div>
-                <ul>
-                    {Object.entries(currentCards).map(([playerId, cards], index) => (
-                        <li key={index} className="mb-4">
-                            <strong>{playerId}:</strong>
-                            <ul className="ml-4">
-                                {cards.map((card, cardIndex) => (
-                                    <li key={cardIndex} className="mb-1">{card}</li>
-                                ))}
-                            </ul>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </>
-    );
-    
+	return (
+		<>
+			<div className="container mx-auto px-4 py-8">
+				<div className="mb-2">Room Code: {location.state.name}</div>
+				<div className="mb-2">
+					Players:{' '}
+					{players.map((playerId, index) => (
+						<li key={index} className="mb-1">
+							{playerId} <strong>(Player {index + 1})</strong>
+						</li>
+					))}
+				</div>
+				<ul className="mb-4"></ul>
+				<div className="mb-2">Cards:</div>
+				<ul>
+					{Object.entries(currentCards).map(([playerId, cards], index) => (
+						<li key={index} className="mb-4">
+							<strong>Player {index + 1}:</strong>
+							<ul className="ml-4">
+								{cards.map((card, cardIndex) => (
+									<li key={cardIndex} className="mb-1">
+										{card}
+									</li>
+								))}
+							</ul>
+						</li>
+					))}
+				</ul>
+			</div>
+		</>
+	);
 }
 
 export default Play;
